77e96b05932f036adb27c86a8b080562cadb2b71
sannahtijani <131191187+sannahtijani@users.noreply.github.com>
2023-12-20 15:58:39 +0100

    loading the data

---
+++ read files sannah.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(doBy)
+library(tidyverse)
+library(Hmisc)
+library(tidyr)
+library(haven)
+library(acid)
+library(StatMatch)
+getwd()
+setwd("C:/Users/ducie/Documents/WU/distribution field/Project")
+data_us16_wi <- read_dta("us16wp (4).dta")
+?read_dta
+
