18caa600deeba3e79435c2e5db417fe5bb3706d6
sannahtijani <131191187+sannahtijani@users.noreply.github.com>
2024-01-02 12:27:58 +0100

    Update try US 16 matching election and wealth data.R
    merging the data based on the hid but issue due to the numb of obs for household is 4980 for only 996 hid

--- try US 16 matching election and wealth data.R
+++ try US 16 matching election and wealth data.R
@@ -30,3 +30,14 @@ anes_data <- read_dta(file.path(extracted_dir, "anes_timeseries_2016.dta"))
 
 # Print the first few rows of the dataset
 head(anes_data)
+# Create a temporary index by counting the hid values
+data_us16_wh$index <- ave(data_us16_wh$hid, data_us16_wh$hid, FUN = seq_along)
+data_us16_wi$index <- ave(data_us16_wi$hid, data_us16_wi$hid, FUN = seq_along)
+
+# Merge the data sets by hid and index, keeping all observations from wi
+merged_data <- merge(data_us16_wh, data_us16_wi, by = c("hid", "index"), all.y = TRUE)
+
+# Drop the temporary index
+merged_data$index <- NULL
+
+#issue is that there are more NA in the dataset because the hid is only 996 for 4980 observations 
